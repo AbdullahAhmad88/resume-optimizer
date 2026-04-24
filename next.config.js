d90f24ba937c29062f6decb5839cb90ff67ab99a
Abdullah Ahmad <abdullahahmadmirza42@gamil.com>
2026-04-24 21:06:31 +0500

    updated next version

--- next.config.js
+++ next.config.js
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    turbo: false, // ❌ disable turbopack
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
